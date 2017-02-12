16764494ed2c6f0d41bb258d840643bedf3c91fb
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-02-12 15:11:11 +0100

    Add basic structure skeleton

---
+++ main.js
@@ -0,0 +1,30 @@
+
+function $id(id, parent) {
+  return (parent || document).getElementById(id);
+}
+function $listen(node, event, handler) {
+  if (typeof node == "string") node = $id(node);
+  node.addEventListener(event, handler);
+}
+
+function showNode(node) {
+  if (typeof node == "string") node = $id(node);
+  if (! node || ! node.classList.contains("pane")) return;
+  var prev = node.previousElementSibling, next = node.nextElementSibling;
+  while (prev) {
+    prev.classList.remove("visible");
+    prev = prev.previousElementSibling;
+  }
+  while (next) {
+    next.classList.remove("visible");
+    next = next.nextElementSibling;
+  }
+  showNode(node.parentNode);
+  node.classList.add("visible");
+}
+
+function init() {
+  showNode("titlescreen");
+}
+
+window.addEventListener("load", init);
